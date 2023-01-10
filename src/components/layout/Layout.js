66d4969c38587deea117a6d055e65b420a5142f1
Assadiki Yassin <yassin-it@pm.me>
2023-01-10 21:05:30 +0100

    feat: Init Main header with content and MoonIcon

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,11 +1,12 @@
 import { Fragment } from "react"
+import MainHeader from "../main-header/MainHeader"
 
 import classes from './Layout.module.css'
 
 const Layout = props => {
 
     return <Fragment>
-
+        <MainHeader />
         <main className={classes['main-container']}>
             {props.children}
         </main>
